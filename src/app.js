f8d2e38773e26c6ad0549d8d6409c626de7e3906
Eduardo Garcia <84125798+edugn2298@users.noreply.github.com>
2024-05-25 00:20:00 -0400

    actualizacion

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const toggleButton = document.getElementById('toggle-mode');
-const contentContainer = document.getElementById('content');
 
 toggleButton.addEventListener('click', () => {
-  contentContainer.classList.toggle('dark:bg-neutral-800'); // Cambia la clase para el modo oscuro
+  document.documentElement.classList.toggle("dark"); // Cambia la clase para el modo oscuro
 });
