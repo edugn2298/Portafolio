380b4c629e7029a6ae5c79de2802e55132afa820
Eduardo Garcia <84125798+edugn2298@users.noreply.github.com>
2024-05-24 20:48:29 -0400

    Base

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'terra': {
+          '50': '#fef3f2',
+          '100': '#fee4e2',
+          '200': '#ffcdc9',
+          '300': '#fdaba4',
+          '400': '#fa7a6f',
+          '500': '#f24f41',
+          '600': '#df3223',
+          '700': '#af2418',
+          '800': '#9b2319',
+          '900': '#80241c',
+          '950': '#460e09',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
